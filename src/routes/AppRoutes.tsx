84c0ee6f4957aa0e035f1ad77eb64debb456d1ec
JordiLara <jordilarareyes@gmail.com>
2024-12-20 17:53:41 +0100

    add an update to landingpage to integrate login   and register components

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
