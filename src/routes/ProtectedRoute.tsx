cb6ff4e7520688eaa7860fc08bf0a275a9fda6bd
JordiLara <jordilarareyes@gmail.com>
2024-12-21 20:41:38 +0100

    Temporary login for protected route

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -4,6 +4,10 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute: React.FC<{children: JSX.Element}> = ({children}) => {
     const {user}= useAuth();
 
+    if (user === null) {
+      return <p>Loading...</p>;
+    }
+
     if (!user) {
         return <Navigate to="/login" />
     }
