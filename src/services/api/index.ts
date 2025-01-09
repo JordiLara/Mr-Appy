f38d0904374c238457c8b3acb1141cabb8ae4cdf
JordiLara <jordilarareyes@gmail.com>
2025-01-09 21:47:02 +0100

    add reviewservice to index

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,4 +1,4 @@
 export { moodService } from "./moodService";
 export { teamService } from "./teamService";
 export { userService } from "./userService";
-
+export {reviewService} from "./reviewService";
