157625792ee3e6527c64f4c9b73f460febcfb294
JordiLara <jordilarareyes@gmail.com>
2025-01-15 21:06:14 +0100

    fixes tree-shaking tailwindcss problem with calendar colors, forcing tailwind to accept those colors even if they are not in the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,32 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: {
+          500: "#10B981",
+        },
+        green: {
+          500: "#22C55E",
+        },
+        yellow: {
+          500: "#EAB308",
+        },
+        orange: {
+          500: "#F97316",
+        },
+        red: {
+          500: "#EF4444",
+        },
+      },
+    },
   },
-  plugins: [],
+  safelist: [
+    "bg-emerald-500",
+    "bg-green-500",
+    "bg-yellow-500",
+    "bg-orange-500",
+    "bg-red-500",
+    "bg-gray-500",
+  ],
 };
-
