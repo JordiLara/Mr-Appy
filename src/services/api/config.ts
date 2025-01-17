f518bf2da692e0bb004887fc23418a848c886c4b
JordiLara <jordilarareyes@gmail.com>
2025-01-17 20:07:29 +0100

    commit

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -8,7 +8,7 @@ const api = axios.create({
   },
 });
 
-  //Request interceptor for adding auth token
+//Request interceptor for adding auth token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
