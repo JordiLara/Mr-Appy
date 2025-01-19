85ee362e3e2d3cafe7b8e565cd31a7f88528fb6a
JordiLara <jordilarareyes@gmail.com>
2025-01-19 18:04:48 +0100

    deleted commented code

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -8,7 +8,6 @@ const api = axios.create({
   },
 });
 
-//Request interceptor for adding auth token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -21,30 +20,5 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-
-// Response interceptor for handling errors
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response) {
-//       Handle 401 Unauthorized
-//       if (error.response.status === 401) {
-//         localStorage.removeItem("token");
-//         window.location.href = "/login";
-//       }
-
-//       Handle 403 Forbidden
-//       if (error.response.status === 403) {
-//         console.error("Access forbidden");
-//       }
-
-//       Handle 500 Server Error
-//       if (error.response.status >= 500) {
-//         console.error("Server error occurred");
-//       }
-//     }
-//     return Promise.reject(error);
-//   }
-// );
 
 export default api;
